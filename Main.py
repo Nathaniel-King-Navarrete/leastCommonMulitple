5317c1f3b9f4c6157645fb555814195dc30a62b9
NathanielNavarrete <nathanielnavarrete@hotmail.com>
2023-08-01 09:50:51 -0700

    Added error handling. The ability to guess on what the lcm is. Show the results at the end.

--- Main.py
+++ Main.py
@@ -1,10 +1,4 @@
-
-
-
-
-
-
-def FindLCM(firstNumber, secondNumber):
+def GuessIsLCM(firstNumber, secondNumber, guess):
     LCM = {}
     biggestNumber = firstNumber
     smallerNumber = secondNumber
@@ -16,22 +10,55 @@ def FindLCM(firstNumber, secondNumber):
     biggestSummer = biggestNumber
     smallerSummer = smallerNumber
 
-    while True:
+    while smallerSummer  <= guess:
         LCM[biggestSummer] = biggestSummer
 
         if smallerSummer not in LCM:
             LCM[smallerSummer] = smallerSummer
+        elif smallerSummer in LCM and smallerSummer == guess:
+            return True
         else:
-            return smallerSummer
+            return False
 
         biggestSummer += biggestNumber
-        smallerSummer += smallerNumber   
+        smallerSummer += smallerNumber
 
+    return False       
+
+def MultiplesUntilLCM(number, LCM):
+    summer = number
+    multiples = ""
+    while summer != LCM:
+        multiples += (str(summer) + ', ')
+        summer += number
+    multiples += str(summer)
+    return multiples
 
 if __name__ == '__main__':
-    firstNumber = 789
-    secondNumber = 342
-    print(FindLCM(firstNumber, secondNumber))
+    firstNumber = input("Enter your first number:\n")
+    while not firstNumber.isdigit():
+        print("Value entered is not an integer.")
+        firstNumber = input("Enter your first number:\n")
+    firstNumber = int(firstNumber)
+
+    secondNumber = input("Enter your second number:\n")
+    while not secondNumber.isdigit():
+        print("Value entered is not an integer.")
+        secondNumber = input("Enter your second number:\n")
+    secondNumber = int(secondNumber)   
+
+    guess = input("Enter your guess:\n")
+    while not guess.isdigit():
+        print("Value entered is not an integer.")
+        guess = input("Enter your guess:\n")
+    guess = int(guess)
+
+    if GuessIsLCM(firstNumber, secondNumber, guess):
+        print(MultiplesUntilLCM(firstNumber, guess))
+        print(MultiplesUntilLCM(secondNumber, guess))
+    else:
+        print("Guess is not LCM.")
+    
 
 
 
